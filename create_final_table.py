fd50f305c46427b14a4e297d1d6b1fddcbe1c3a2
skidanb123linkoln-red <skidanb123@gmail.com>
2021-03-09 16:35:57 +0200

    Refactor create_final_table.py

--- create_final_table.py
+++ create_final_table.py
@@ -1,13 +1,15 @@
 import glob
 import xml.etree.ElementTree as ET
 import pandas as pd
+
 xml_list = glob.glob("xml/*.xml")
 
-df = pd.DataFrame(columns = ['Номер', 'Площа'])
+df = pd.DataFrame(columns=['Номер', 'Площа'])
 for xml in xml_list:
     print(xml)
     tree = ET.parse(xml)
-    qadNum = tree.find(".//CadastralZoneNumber").text+":"+tree.find(".//CadastralQuarterNumber").text+":"+ tree.find(".//ParcelID").text
+    qadNum = tree.find(".//CadastralZoneNumber").text + ":" + tree.find(
+        ".//CadastralQuarterNumber").text + ":" + tree.find(".//ParcelID").text
     square = tree.find(".//Size").text
     df.loc[len(df.index)] = [qadNum, square]
 df.to_csv("table.csv")
